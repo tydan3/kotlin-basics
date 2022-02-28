0f4aa3b68e756cea19af12f64f66f96b675a4d1d
Daniel Ty <tydaniel270@gmail.com>
2022-02-27 19:21:40 -0800

    Created navigation test for words app

--- Unit-3-Navigation/android-basics-kotlin-words-app-starter/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ Unit-3-Navigation/android-basics-kotlin-words-app-starter/app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,37 +1,43 @@
 package com.example.wordsapp
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Rule
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
-    @get:Rule
-    val activity = ActivityScenarioRule(MainActivity::class.java)
-
     @Test
-    fun navigate_to_word() {
-        // select list item
-        // by text
-//        onView(withText("C")).perform(click())
+    fun navigate_to_words_nav_component() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(themeResId =
+        R.style.Theme_Words)
 
-        // or by adapter position
+        letterListScenario.onFragment { fragment ->
+
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
         onView(withId(R.id.recycler_view))
             .perform(
                 RecyclerViewActions
-                    .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click())
-            )
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
 
-        // check that correct screen is displayed by checking text in title
-        onView(withText("Words That Start With C")).check(matches(isDisplayed()))
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+
     }
 }
